3e0ecfba80e2d15baaeca48b6bf818a00c990573
Kei <budi03@gmail.com>
2024-06-22 01:14:29 +0700

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,10 @@
 <?php
 
 use Realodix\Relax\Config;
+use Realodix\Relax\RuleSet\Sets\Realodix;
 
 $localRules = [
     // ...
 ];
 
-return Config::create('@Realodix', $localRules);
+return Config::create(new Realodix, $localRules);
